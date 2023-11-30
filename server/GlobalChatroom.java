4b2b2b16097b124f36cecbfe746c4c2f41d7f965
Richard van Leeuwen <r.e.van.leeuwen@student.vu.nl>
2023-11-30 15:42:00 +0200

    updated command list

--- server/GlobalChatroom.java
+++ server/GlobalChatroom.java
@@ -16,51 +16,35 @@ public class GlobalChatroom extends Chatroom {
 
     @Override
     protected void processCommand(String command) {
-        String[] messageParts = command.split(":",2);
-        if(messageParts[1].startsWith(" /")) { 
-            String[] commandString = messageParts[1].split(" ", 4); //best stripped but works fine for now
-            if(commandString[1].equals("/users")) { //considering a switch with functions for later
-                DataOutputStream stream = outputStreams.get(messageParts[0]);
-                try {
-                    stream.writeUTF(getUserNames());
-                    stream.flush();
+        String[] splitCommand = command.split(":",2);
+        if(splitCommand[1].startsWith(" /")) { 
+            String[] commandParts = splitCommand[1].split(" ", 4); //best stripped but works fine for now
+            switch(commandParts[1]) {
+                case "/users":
+                    executeUsersCommand(splitCommand[0]);
                     return;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } 
-            }
-            if(commandString[1].equals("/whisper")) { //TODO: clean up error checking and modulise into functions
-                if(commandString.length < 3) {
-                    DataOutputStream stream = outputStreams.get(messageParts[0]);
+                case "/whisper":
+                    executeWhisperCommand(commandParts, splitCommand[0], commandParts[2], command);
+                    return;
+                case "/createChatroom":
+                    return;
+                case "/joinChatroom":
+                    return;
+                default:
+                    DataOutputStream stream = outputStreams.get(splitCommand[0]);
                     try {
-                        stream.writeUTF("Please specify whisper target");
+                        stream.writeUTF("Invalid command");
                         stream.flush();
                         return;
                     } catch (IOException e) {
                         e.printStackTrace();
-                    }
-                }
-                DataOutputStream stream = outputStreams.get(commandString[2]);
-                if(stream == null) {
-                    stream = outputStreams.get(messageParts[0]);
-                    try {
-                        stream.writeUTF("Invalid target");
-                        stream.flush();
                         return;
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
-                }
-                try {
-                    stream.writeUTF(command);
-                    stream.flush();
-                    return;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
             }
         }
-        broadcastMessage(command, messageParts[0]);
+        else {
+            broadcastMessage(command, splitCommand[0]);
+        }
     }
 
 
