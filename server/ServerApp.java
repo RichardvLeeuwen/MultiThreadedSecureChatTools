feea6bc8ecfb8a9c01a838cdc0fb88f268b58559
Richard van Leeuwen <r.e.van.leeuwen@student.vu.nl>
2024-01-23 04:20:15 +0200

    SSL Working

--- server/ServerApp.java
+++ server/ServerApp.java
@@ -1,36 +1,37 @@
 package server;
+
+
 import java.net.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+
+import javax.net.ssl.SSLSocket;
+
+
 import helper.*;
 
 public class ServerApp {
     private static final int PORT = 5000;
-   
+    private static final String KEYSTOREPATH = "C:\\Users\\Richard\\Desktop\\MultiThreadedSecureChatTools\\RichChatServerKeyStore.jks";
+    private static final char[] PASSWORD = "password".toCharArray();
     public static void main(String[] args) throws Exception {
 
-        
-
         System.out.println("Booting up server");
-        ServerSocket serverSocket = new ServerSocket(PORT);
-
-        
-
+        ServerSocket serverSocket = TLSWrapper.returnTLSServerSocket(KEYSTOREPATH, PASSWORD, PORT);
+        if (serverSocket == null) return; 
         List<Client> newClients =  new ArrayList<Client> ();
         Thread globalChatroomThread = new Thread(new GlobalChatroom("Global", newClients));
         globalChatroomThread.start();
 
-
-        ConcurrentLinkedQueue<Socket> loginQueue = new ConcurrentLinkedQueue<Socket>();
+        ConcurrentLinkedQueue<SSLSocket> loginQueue = new ConcurrentLinkedQueue<SSLSocket>();
         Thread loginThread = new Thread(new LoginHandler(loginQueue, newClients));
         loginThread.start();
 
 
-
         while(true) {
-            Socket clientSocket = serverSocket.accept();
+            SSLSocket clientSocket = (SSLSocket) serverSocket.accept();
             loginQueue.offer(clientSocket);
             
         }
